545ba8b267c3b0974e51f33a447db5466460bee8
Jamie Holliday <jamie@jamieholliday.co.uk>
2014-03-08 17:30:15 +0000

    Added search all function

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -3,7 +3,7 @@
 angular.module('nodeboxApp')
 	.controller('SearchCtrl', function ($scope, $http) {
 		$scope.search = function() {
-			$http.post('api/search', {'data': $scope.searchTerm})
+			$http.post('api/searchAll', {'searchTerm': $scope.searchTerm})
 			.success(function(data, status) {
 				$scope.status = status;
 				$scope.result = data;

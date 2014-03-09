afa03fc45a7788f287deee7f59c6571f8c686a3e
Jamie Holliday <jamie@jamieholliday.co.uk>
2014-03-09 13:13:53 +0000

    Added atyling for search page. Added search results for Spotify to search page

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -2,11 +2,62 @@
 
 angular.module('nodeboxApp')
 	.controller('SearchCtrl', function ($scope, $http) {
+		$scope.tracks = [
+			{
+				title: ['title1'],
+				artist: ['artist1'],
+				album: ['album1']
+			},
+			{
+				title: ['title2'],
+				artist: ['artist2'],
+				album: ['album1']
+			},
+			{
+				title: ['title3'],
+				artist: ['artist3'],
+				album: ['album1']
+			}
+		];
+
+		$scope.artists = [
+			{
+				name: ['name1'],
+			},
+			{
+				name: ['name2'],
+			},
+			{
+				name: ['name3'],
+			}
+		];
+
+		$scope.albums = [
+			{
+				name: ['name1'],
+				'artist-name': ['artist-name1']
+			},
+			{
+				name: ['name2'],
+				'artist-name': ['artist-name2']
+			},
+			{
+				name: ['name3'],
+				'artist-name': ['artist-name3']
+			}
+		];
+
 		$scope.search = function() {
 			$http.post('api/searchAll', {'searchTerm': $scope.searchTerm})
 			.success(function(data, status) {
 				$scope.status = status;
+				$scope.tracks = data.result.tracks[0].track;
+				$scope.artists = data.result.artists[0].artist;
+				$scope.albums = data.result.albums[0].album;
+				// $scope.albums = data.result.tracks[0].track;
+
 				$scope.result = data;
+				console.log(data.result.tracks[0].track);
 			})
 			.error(function(data, status) {
 				$scope.data = data || 'Sorry couldn\'t get \'owt';
@@ -14,3 +65,4 @@ angular.module('nodeboxApp')
 			});
 		};
 });
+
